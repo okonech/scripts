99b4673082bdc6bbb067ee4801177d18a9bc803c
Alex Okonechnikov <alexo45@gmail.com>
2020-10-26 13:59:57 -0400

    a

--- gcp-port-forward.sh
+++ gcp-port-forward.sh
@@ -1,5 +1,41 @@
 #! /bin/bash
-gcloud container clusters get-credentials meditech-cluster-1 --zone us-east4-b --project cloud-services-dev
+
+set -o errexit # exit on errors
+set -o nounset # exit on use of uninitialized variable
+
+CLUSTER=stable-cluster-1
+
+if [ $# -eq 0 ]; then
+    echo "Defaulting to cloud stable instance"
+fi
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    shift
+    # shifts to skip ... --{arg} {val} ...
+    case $key in
+    stable)
+        CLUSTER=stable-cluster-1
+        echo Connecting to cloud stable instance
+        ;;
+    dev)
+        echo Connecting to cloud dev instance
+        CLUSTER=meditech-cluster-1
+        ;;
+    -*)
+        echo ERROR - unknown option: $key
+        displayHelp
+        exit 1
+        ;;
+    *)
+        echo ERROR - unknown argument: $key
+        displayHelp
+        exit 1
+        ;;
+    esac
+done
+
+gcloud container clusters get-credentials $CLUSTER --zone us-east4-b --project cloud-services-dev
 kubectl port-forward svc/rest-appserver 8081:8081 &
 APP_PF_PID=$!
 echo Application Port Forwarding Process ID: $APP_PF_PID
