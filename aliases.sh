13ce6d91f51fbe93db472ffe588bab22e33a925e
Alex Okonechnikov <alexo45@gmail.com>
2021-10-28 11:40:18 -0400

    a

--- aliases.sh
+++ aliases.sh
@@ -47,7 +47,7 @@ alias jenkins="gcloud compute ssh --project=nth-fort-242316 --zone=us-east4-c re
 
 #git
 alias gcm="git-fetch && git checkout main && git pull"
-alias grim="git-fetch && && git rebase -i origin/main"
+alias grim="git-fetch && git rebase -i origin/main"
 alias gpf="git-fetch && git push --force-with-lease"
 alias recent="git for-each-ref --sort=-committerdate --count=10 --format='%(refname:short)' refs/heads/"
 # navigate to top of repo
